f4f7e7e2628c2563f4937414d7c879afd4280907
young wang <wangyiang@whu.edu.cn>
2019-03-27 20:14:24 +0800

    update

---
+++ Artificial_Inteligence/Activity-dose/Code/activation.py
@@ -0,0 +1,72 @@
+# -*- coding=utf-8 -*-
+# Author: Y'A'Wang
+# Date:
+# Aim：
+from __future__ import absolute_import, division, print_function
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from keras.layers import *
+from keras.models import Model, load_model
+from keras.optimizers import Adam
+
+
+# load data and model
+X = np.load("MP_Lab\\Artificial_Inteligence\\Activity-dose\\data\\X.npy")
+Y = np.load("MP_Lab\\Artificial_Inteligence\\Activity-dose\\data\\Y.npy")
+model = load_model("MP_Lab\\Artificial_Inteligence\\Activity-dose\\data\\rnn_model_0903.h5")
+
+
+# 以这个model的预测值作为输出
+extract_state_model = Model(
+    inputs=model.input,
+    outputs=model.get_layer('lstm_1').output
+)
+h_state = extract_state_model.predict(X[:, :, np.newaxis])
+
+
+y_predict = model.predict(X[:, ::-1, np.newaxis])
+
+x = np.concatenate((X[:, 50:100], X[:, 0:50], X[:, 100:302]), axis=1)
+y = np.concatenate((Y[:, 50:100], Y[:, 0:50], Y[:, 100:302]), axis=1)
+y_predict = model.predict(x[:, :, np.newaxis])
+
+
+# Input a 1D array.
+def normalize(data):
+    mx = max(data)
+    mn = min(data)
+    return [(float(i) - mn) / (mx-mn) for i in data]
+
+# 反向
+c = np.arange(0, 302)
+plt.plot(c, normalize(y_predict[1, :]), linestyle='--')
+plt.plot(c, X[1, ::-1], linestyle='--')
+plt.plot(c, Y[1, ::-1], linestyle='--')
+plt.show()
+# 切片调整
+c = np.arange(0, 302)
+plt.plot(c, normalize(y_predict[1, :]), linestyle='--')
+plt.plot(c, x[1, :], linestyle='--')
+plt.plot(c, y[1, :], linestyle='--')
+plt.show()
+
+# plot activation
+fig, ax = plt.subplots(figsize=(40, 20))
+b = np.swapaxes(h_state[14], 0, 1)
+c = ax.pcolor(b, cmap='RdBu', vmin=-1, vmax=1)
+ax.set_xticklabels([0, 50, 100, 150, 200, 250, 300], fontsize=40) # 坐标轴的刻度
+ax.set_yticklabels([0, 5, 10, 15, 20, 25, 30], fontsize=40)
+ax.set_xlabel('Depth', fontsize=50)
+ax.set_ylabel('Units', fontsize=50)
+#ax0.set_title('default: no edges')
+#fig.subplots_adjust(right=0.8)
+#cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
+e = fig.colorbar(c, ax=ax)
+e.ax.tick_params(labelsize='large')  # 调整 coloebar 字体的
+# 带网格线的
+# c = ax[1].pcolor(b, edgecolors='k', linewidths=1)
+# ax1.set_title('thick edges')
+#fig.tight_layout()
+plt.show()
+
--- Artificial_Inteligence/Flat_Panel_with_Collimator/Code/main.py
+++ Artificial_Inteligence/Flat_Panel_with_Collimator/Code/main.py
@@ -9,121 +9,44 @@ from keras.optimizers import Adam
 from keras.preprocessing import sequence
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import interpolate
 
+"""
+Notice: Modify "\\" into "/" in Linux server!
+X = np.load("MP_Lab/Artificial_Inteligence/Flat_Panel_with_Collimator/data/collimator_H3_X.npy")
+Y = np.load("MP_Lab/Artificial_Inteligence/Flat_Panel_with_Collimator/data/collimator_H3_Y.npy")
+"""
 
-# Notice: Modify "\\" into "/" in Linux server!
-# X = np.load("MP_Lab/Artificial_Inteligence/Flat_Panel_with_Collimator/data/collimator_H3_X.npy")
-# Y = np.load("MP_Lab/Artificial_Inteligence/Flat_Panel_with_Collimator/data/collimator_H3_Y.npy")
 X = np.load("MP_Lab\\Artificial_Inteligence\\Flat_Panel_with_Collimator\\data\\collimator_H3_X.npy")
 Y = np.load("MP_Lab\\Artificial_Inteligence\\Flat_Panel_with_Collimator\\data\\collimator_H3_Y.npy")
 X1 = X[:, 0:150:2]
-X2 = X[:, 1:150:2]
-X3 = (X1 + X2)/2
-x_train = X[:, :, np.newaxis]
-y_train = Y[:, :, np.newaxis]
-
-import random
-import json
-from six.moves import range
-import six
-
-
-def sequence_pad(sequences, maxlen=None, dtype='int32',
-                  padding='pre', truncating='pre', value=0.):
-    """Pads sequences to the same length.
-
-    `(num_samples, num_timesteps)`.
-
-    `num_timesteps` is either the `maxlen` argument if provided,
-    or the length of the longest sequence otherwise.
-
-    Sequences that are shorter than `num_timesteps`
-    are padded with `value` at the end.
-
-    # Arguments
-        sequences: List of lists, where each element is a sequence.
-        maxlen: Int, maximum length of all sequences.
-        dtype: Type of the output sequences.
-            To pad sequences with variable length strings, you can use `object`.
-        padding: String, 'pre' or 'post':
-            pad either before or after each sequence.
-        value: Float or String, padding value.
-
-    # Returns
-        x: Numpy array with shape `(num_samples, maxlen)`
-
-    # Raises
-        ValueError: In case of invalid values for `truncating` or `padding`,
-            or in case of invalid shape for a `sequences` entry.
-    """
-    if not hasattr(sequences, '__len__'):
-        raise ValueError('`sequences` must be iterable.')
-    lengths = []
-    for x in sequences:
-        if not hasattr(x, '__len__'):
-            raise ValueError('`sequences` must be a list of iterables. '
-                             'Found non-iterable: ' + str(x))
-        lengths.append(len(x))
-
-    num_samples = len(sequences)
-    if maxlen is None:
-        maxlen = np.max(lengths)
-
-    # take the sample shape from the first non empty sequence
-    # checking for consistency in the main loop below.
-    sample_shape = tuple()
-    for s in sequences:
-        if len(s) > 0:
-            sample_shape = np.asarray(s).shape[1:]
-            break
-
-    is_dtype_str = np.issubdtype(dtype, np.str_) or np.issubdtype(dtype, np.unicode_)
-    if isinstance(value, six.string_types) and dtype != object and not is_dtype_str:
-        raise ValueError("`dtype` {} is not compatible with `value`'s type: {}\n"
-                         "You should set `dtype=object` for variable length strings."
-                         .format(dtype, type(value)))
-
-    x = np.full((num_samples, maxlen) + sample_shape, value, dtype=dtype)
-    for idx, s in enumerate(sequences):
-        if not len(s):
-            continue  # empty list/array was found
-        if truncating == 'pre':
-            trunc = s[-maxlen:]
-        elif truncating == 'post':
-            trunc = s[:maxlen]
-        else:
-            raise ValueError('Truncating type "%s" '
-                             'not understood' % truncating)
-
-        # check `trunc` has expected shape
-        trunc = np.asarray(trunc, dtype=dtype)
-        if trunc.shape[1:] != sample_shape:
-            raise ValueError('Shape of sample %s of sequence at position %s '
-                             'is different from expected shape %s' %
-                             (trunc.shape[1:], idx, sample_shape))
-
-        if padding == 'post':
-            x[idx, :len(trunc)] = trunc
-        elif padding == 'pre':
-            x[idx, -len(trunc):] = trunc
-        else:
-            raise ValueError('Padding type "%s" not understood' % padding)
-    return x
 
 
 # pre是在前填充， post是在后填充；
-x_train = sequence.pad_sequences(x_train, maxlen=299, padding='post', value=-1)
+# dtype default: int32; dtype项务必要改成float！
+X1 = sequence.pad_sequences(X1, maxlen=299, padding='post', value=0, dtype="float")
+Y = sequence.pad_sequences(Y, maxlen=299, padding='post', value=0, dtype="float")
+X2 = X[:, 1:150:2]
+X3 = (X1 + X2)/2
+x_train = X1[:, :, np.newaxis]
+y_train = Y[:, :, np.newaxis]
+# 插值
+x1o = np.linspace(0, 75, 75)
+x1n = np.linspace(0, 75, 299)
+y1n = np.zeros((40, 299))
+for i in range(40):
+    f = interpolate.interp1d(x1o, X1[i, :], kind="cubic", )
+    y1n[i, :] = f(x1n)
 
 
 # Test Set (Based on the requirement of research)
-
 x_test = x_train
 y_test = y_train
 
 
 # Create model.
 model = Sequential()
-model.add(Masking(mask_value=-1, input_shape=(299, 1)))
+model.add(Masking(mask_value=0, input_shape=(None, 1)))
 model.add(LSTM(
         units=10,
         return_sequences=True,       # True: output at all steps. False: output as last step.
@@ -131,6 +54,7 @@ model.add(LSTM(
 ))
 # add output layer
 model.add(TimeDistributed(Dense(1)))
+model.add(Masking(mask_value=0))
 # Set Optimizer
 opt = Adam(lr=0.001, decay=1e-6)
 model.compile(optimizer=opt,
